e5d9bafc79d5f8446c637261181176379bbe91b9
Rybin Andrey <rybin.ag@gmail.com>
2021-12-14 15:15:12 +0300

    ruby tournament ex added

--- ruby/two-bucket/two_bucket.rb
+++ ruby/two-bucket/two_bucket.rb
@@ -5,26 +5,26 @@ Write your code for the 'Two Bucket' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/two-bucket` directory.
 =end
-require 'pry'
+
 
 class TwoBucket
-    @@move = 0;
+    
     def initialize(first_bucket_size, second_bucket_size, desired_liters, full_bucket)
-        if full_bucket == "one"
-            @should_be_full = Bucket.new(first_bucket_size, "one")
-            @should_be_empty = Bucket.new(second_bucket_size, "two")
-        else
-            @should_be_full = Bucket.new(second_bucket_size, "two")
-            @should_be_empty = Bucket.new(first_bucket_size, "one")
-        end
+
+        @should_be_full = Bucket.new(first_bucket_size, "one")
+        @should_be_empty = Bucket.new(second_bucket_size, "two")
+
         @desired_liters = desired_liters
         @full_bucket = full_bucket
+        
+        @should_be_full, @should_be_empty = @should_be_empty, @should_be_full if full_bucket == "two"        
     end
 
 
     def moves        
         count = 0        
         while @should_be_full.curent_volume != @desired_liters && @should_be_empty.curent_volume != @desired_liters
+
             if @should_be_full.is_empty?
                 @should_be_full.fill_bucket
             elsif @should_be_empty.is_full?
@@ -35,7 +35,7 @@ class TwoBucket
                 @should_be_full.pour_into_other(@should_be_empty)
             end
             count += 1
-            #binding.pry
+           
         end
         count        
     end
@@ -54,45 +54,41 @@ class TwoBucket
             @full_volume = full_volume
             @curent_volume = 0
             @name = name
-        end
-
-        def move
-            @@move
-        end
+        end        
 
         def is_empty?
-            self.curent_volume == 0
+            curent_volume == 0
         end
 
         def is_full?
-            self.curent_volume == self.full_volume 
+            curent_volume == full_volume 
         end
 
         def fill_bucket
-            @curent_volume = @full_volume            
+            @curent_volume = full_volume            
         end
 
         def empty_bucket
             @curent_volume = 0            
         end
 
+        def can_receive?(free_space, liters_into)        
+            free_space > liters_into
+        end
+
         def pour_into_other(into_bucket)
-            into_bucket_emptyness = into_bucket.full_volume - into_bucket.curent_volume
-            can_fill_in = into_bucket_emptyness - self.curent_volume
-            #binding.pry
-            if can_fill_in > 0
-                into_bucket.curent_volume += self.curent_volume
+
+            free_space = into_bucket.full_volume - into_bucket.curent_volume            
+           
+            if can_receive? free_space, curent_volume
+                into_bucket.curent_volume += curent_volume
                 self.curent_volume = 0
             else
                 into_bucket.curent_volume = into_bucket.full_volume
-                self.curent_volume -=into_bucket_emptyness
+                self.curent_volume -= free_space
             end
         end
 
     end
 end
 
-#game = TwoBucket.new(3,5,1,'one')
-#puts game.moves
-#puts game.goal_bucket
-#puts game.other_bucket
