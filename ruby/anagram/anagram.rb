b82e501b1ae66fcdff264a53e6b740e20d526053
FunnyDrew <rybin.ag@gmail.com>
2022-02-08 07:57:41 +0300

    anagram solve

--- ruby/anagram/anagram.rb
+++ ruby/anagram/anagram.rb
@@ -5,3 +5,27 @@ Write your code for the 'Anagram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/anagram` directory.
 =end
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def word
+        @word.downcase
+    end
+
+    def to_h(word)
+        word.split("").reduce({}) do |acc, ch|
+            acc.include?(ch) ? acc[ch] +=1 : acc[ch] = 1
+            acc
+        end
+    end
+
+    def match(words)
+        main = self.to_h(word)
+        words.select do |w|
+            w.downcase == word ? false : self.to_h(w.downcase) == main
+        end
+    end
+end
+
