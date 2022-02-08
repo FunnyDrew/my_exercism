10e48cc77dc28ee91fcd1bb2ee285354644af80b
FunnyDrew <rybin.ag@gmail.com>
2022-02-08 21:40:57 +0300

    mantrix solved

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -5,3 +5,25 @@ Write your code for the 'Matrix' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/matrix` directory.
 =end
+
+class Matrix
+    def initialize(string)
+        @string_matrix = string
+        @matrix = self.to_matrix
+    end
+
+    def to_matrix
+        @string_matrix.split("\n").reduce([]) do |matrix, row|            
+            data = row.split(" ").map{|num| num.to_i}
+            matrix.push(data)            
+        end
+    end
+
+    def rows
+        @matrix
+    end
+
+    def columns
+        @matrix.transpose
+    end
+end
