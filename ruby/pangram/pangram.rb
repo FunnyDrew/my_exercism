f8bb228bf3c13a10338dc1708a63e03f04d7518d
FunnyDrew <rybin.ag@gmail.com>
2022-02-10 07:06:05 +0300

    pangram solved

--- ruby/pangram/pangram.rb
+++ ruby/pangram/pangram.rb
@@ -5,3 +5,23 @@ Write your code for the 'Pangram' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/pangram` directory.
 =end
+class Pangram
+    
+    def self.pangram?(sentence)
+        @sentence = sentence
+        arrayed_sentence = normalize_sentence.split("")
+        filtered = alphabet.select  do |letter|
+            arrayed_sentence.include?(letter)
+        end
+        filtered == alphabet
+    end
+
+    def self.alphabet
+        ("a".."z").to_a 
+    end
+
+    def self.normalize_sentence
+        @sentence.gsub(/\s+/, "").downcase
+    end
+end
+
