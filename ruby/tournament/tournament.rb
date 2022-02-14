0fb794274a96bb3e25b4385c7c714fb4894a1340
FunnyDrew <rybin.ag@gmail.com>
2022-02-14 07:23:22 +0300

    tournament solving

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -6,14 +6,27 @@ To get started with TDD, see the `README.md` file in your
 `ruby/tournament` directory.
 =end
 class Tournament
-  def self.tally(input)
-    
-    text = <<~Tally
-      Team                           | MP |  W |  D |  L |  P
-    Tally
-    
-    text
+  def self.tally(input)    
+    head = "Team".ljust(31)<<"| MP |  W |  D |  L |  P\n" 
+    team_board=[]
+    team1, team2= parse(input)
   end
-end
 
-puts Tournament.tally("hi");
+  def self.parse(str)
+    team1_name, team2_name, result = str.split(";")
+        
+    if result.strip == "draw"
+      team1 = {name: team1_name.strip, MP: 1, W: 0, D: 1, L: 0, P: 1}
+      team2= {name: team2_name.strip, MP: 1, W: 0, D: 1, L: 0, P: 1}
+    elsif result.strip == "win"
+      team1 = {name: team1_name.strip, MP: 1, W: 1, D: 0, L: 0, P: 3}
+      team2= {name: team2_name.strip, MP: 1, W: 0, D: 0 L: 1, P: 0}
+    else
+      team1 = {name: team1_name.strip, MP: 1, W: 0, D: 0, L: 1, P: 0}
+      team2= {name: team2_name.strip, MP: 1, W: 1, D: 0 L: 0, P: 3}
+    end
+    [team1, team2]
+  end
+
+end
+puts Tournament.tally("tin; bin; win")
