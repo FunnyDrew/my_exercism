d67b45e0d82116aa2388d10b8d20d5fd876f15f6
FunnyDrew <rybin.ag@gmail.com>
2022-02-14 07:59:19 +0300

    else

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -14,19 +14,20 @@ class Tournament
 
   def self.parse(str)
     team1_name, team2_name, result = str.split(";")
-        
+
     if result.strip == "draw"
       team1 = {name: team1_name.strip, MP: 1, W: 0, D: 1, L: 0, P: 1}
       team2= {name: team2_name.strip, MP: 1, W: 0, D: 1, L: 0, P: 1}
     elsif result.strip == "win"
       team1 = {name: team1_name.strip, MP: 1, W: 1, D: 0, L: 0, P: 3}
-      team2= {name: team2_name.strip, MP: 1, W: 0, D: 0 L: 1, P: 0}
+      team2 = {name: team2_name.strip, MP: 1, W: 0, D: 0, L: 1, P: 0}
     else
       team1 = {name: team1_name.strip, MP: 1, W: 0, D: 0, L: 1, P: 0}
-      team2= {name: team2_name.strip, MP: 1, W: 1, D: 0 L: 0, P: 3}
+      team2= {name: team2_name.strip, MP: 1, W: 1, D: 0, L: 0, P: 3}
     end
     [team1, team2]
   end
 
 end
-puts Tournament.tally("tin; bin; win")
+
+puts Tournament.tally("tin; bin; lost")
