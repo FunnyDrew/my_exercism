8fecadfce48c19b834da41e2c543d931a5ef48d1
FunnyDrew <rybin.ag@gmail.com>
2023-09-07 21:52:56 +0300

    ruby flatten_array solution

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -5,3 +5,21 @@ Write your code for the 'Flatten Array' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/flatten-array` directory.
 =end
+
+class FlattenArray
+  def self.flatten(list)
+   result = []
+   self.array_recorder(result, list)
+   result
+  end
+
+  def self.array_recorder(acc, array)
+    array.map do |item|
+      if item.is_a? Array
+        self.array_recorder(acc, item)      
+      elsif item != nil
+        acc<<item
+      end  
+    end
+  end
+end
