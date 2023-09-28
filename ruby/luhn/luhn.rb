bd51ea790d8d4ab15cf82f8df6d2c57e7cc8a89f
FunnyDrew <rybin.ag@gmail.com>
2023-09-28 11:47:45 +0300

    use delete method in luhn.ru

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -5,21 +5,23 @@ Write your code for the 'Luhn' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/luhn` directory.
 =end
-class Luhn
-  def self.doubling(num)
+module Luhn
+  def self.seconds_converting(num)
     2*num >= 9 ? 2*num - 9 : 2*num
   end
 
   def self.valid?(number)
-    normalized = number.chars.select{|item| item != ' '}
+    normalized = number.delete(' ').chars
+
     return false if normalized.length < 2
     
     return false if normalized.select {|item| !(/^[0-9]/ =~ item)}.any?
+
     sum = normalized
     .reverse
     .map(&:to_i)
     .map
-    .with_index {|item, index| index.even? ? item : self.doubling(item)}
+    .with_index {|item, index| index.even? ? item : seconds_converting(item)}
     .sum
     sum % 10 == 0 ? true : false
   end
