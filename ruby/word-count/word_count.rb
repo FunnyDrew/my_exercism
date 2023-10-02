be3d9e6fee42ef66e3dda66c926a91e0b2bd6073
FunnyDrew <rybin.ag@gmail.com>
2023-10-02 07:17:09 +0300

    solve word-count.rb

--- ruby/word-count/word_count.rb
+++ ruby/word-count/word_count.rb
@@ -5,3 +5,20 @@ Write your code for the 'Word Count' exercise in this file. Make the tests in
 To get started with TDD, see the `README.md` file in your
 `ruby/word-count` directory.
 =end
+class Phrase
+  def initialize (words)
+    @phrase = words
+  end
+  def word_count
+    @phrase.downcase
+    .gsub(/'/,'_')
+    .gsub(/[\W]/,' ')
+    .gsub(/_/,'\'')
+    .gsub(/\B'/, ' ')
+    .gsub(/'\B/, ' ')
+    .split(' ').each_with_object({}) do |word, acc|
+      acc[word] ||= 0
+      acc[word] += 1
+    end
+  end
+end
