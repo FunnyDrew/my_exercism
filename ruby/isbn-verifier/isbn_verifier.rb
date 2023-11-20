5548b78908d4f8448abe27b95ccbb295f8ed2bee
FunnyDrew <rybin.ag@gmail.com>
2023-11-20 07:42:32 +0300

    add some ex

---
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -0,0 +1,29 @@
+=begin
+Write your code for the 'ISBN Verifier' exercise in this file. Make the tests in
+`isbn_verifier_test.rb` pass.
+
+To get started with TDD, see the `README.md` file in your
+`ruby/isbn-verifier` directory.
+=end
+
+module IsbnVerifier
+
+	def self.is_X? char
+		char == 'X'
+	end
+
+	def self.prevalid?(isbn_code)
+		
+	end
+
+	def self.valid?(isbn_code)
+		str_without_def = isbn_code.delete('-').chars
+		indexes = str_without_def
+			.each_with_index.map do |value, index|
+				d_index = self.is_X?(value) ? 10 : value.to_i
+				d_index * (10 - index)			
+			end
+
+		indexes.sum % 11 == 0
+	end
+end
