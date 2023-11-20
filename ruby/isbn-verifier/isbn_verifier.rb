a6b617e1afae75538d58e8ba91846cf5b3a82567
FunnyDrew <rybin.ag@gmail.com>
2023-11-20 17:34:52 +0300

    solve isbn-verifier

--- ruby/isbn-verifier/isbn_verifier.rb
+++ ruby/isbn-verifier/isbn_verifier.rb
@@ -13,11 +13,12 @@ module IsbnVerifier
 	end
 
 	def self.prevalid?(isbn_code)
-		isbn_code.delete('-') =~ /\d{9}(\d|X)/ ? true : false
+		isbn_code.delete('-') =~ /\b\d{9}(\d|X)\b/ ? true : false
 	end
 
 	def self.valid?(isbn_code)
 		return false unless self.prevalid?(isbn_code)
+		
 		str_without_def = isbn_code.delete('-').chars
 		indexes = str_without_def
 			.each_with_index.map do |value, index|
